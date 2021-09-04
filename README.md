1b284e2f6dd3a3b01936724436c0e592c3ec1150
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2021-09-03 20:39:41 -0600

    fixing README formatting

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # jason-melick-p0  
 ## User Stories  
 
-##Customers  
+###Customers  
 Customer wants to set up new login  
 Customer wants to log on to app (can set up own log in. must have existing account and provide last 4 of social)  
 *Customer wants to view their balance  
@@ -12,11 +12,10 @@ Customer wants to view their FICO score
 Customer wants to apply for a loan   
 Customer wants to view loan payments  
 
-#Loan Officers  
-Loan officer logins are   
+###Loan Officers  
+Loan officer logins are set by admin/sysop
 Loan officer wants to view pending loan applications (populate with amount, customer's FICO)  
 Loan officer wants to view applicant's balance history (90 days) and any existing loans (balance/payment)  
 Loan officer wants to approve/deny existing applications  
 Set monthly payment and remaining balance automatically after approval  
 Remove from queue whether approved or denied  
-
---
+++ src/main/java/models/utils/ConnectionUtil.java
@@ -0,0 +1,75 @@
+package models.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+
+    private static ConnectionUtil cu = null;
+    private static Properties props;
+
+    private ConnectionUtil(){
+        props = new Properties();
+
+        // need a way to capture data from properties file
+        InputStream dbProps = ConnectionUtil.class.getClassLoader().getResourceAsStream("connection.properties");
+
+        // try stream capture inside try/catch block to catch possible IOException
+        try{
+            // capturing properties from connection.properties from stream
+            props.load(dbProps);
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Problem encountered capturing DB properties from connection.properties");
+        }
+    }
+
+    // method to establish connection to AWS DB
+    // check for existing connection first
+    public static ConnectionUtil getConnectionUtil(){
+        if(cu == null){
+            // is there an existing connection? If not, create a new one.
+            cu = new ConnectionUtil();
+        }
+        // if there is an established connection, return it instead
+        return cu;
+    }
+
+    public Connection getConnection() {
+        // clear any possible existing connection data
+        Connection conn = null;
+
+        // get needed credentials from the connection.properties file
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+
+        // the connection will be passing SQL statements, so we need to catch possible SQL exceptions
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQL Exception encountered while establishing DB connection.");
+        }
+        return conn;
+    }
+
+    public static void main(String[] args){
+
+        Connection connection = getConnectionUtil().getConnection();
+
+        // check for successful connection
+        if(connection != null){
+            System.out.println("DB connection successful.");
+        }else{
+            System.out.println("The attempt to establish a DB was unsuccessful.");
+        }
+    }
+}
+
+
+
